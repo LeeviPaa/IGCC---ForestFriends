7e5772e7292d2404a97211fd02b8d0932ce61218
MasujimaRyohei <nushibeatbox@gmail.com>
2017-09-08 10:23:29 +0900

    I changed creture's mesh to sprite

--- Assets/Scripts/TestFood.cs
+++ Assets/Scripts/TestFood.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestFood : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class TestFood : A_GrabbableObject
+{
+    public override void GetGrabbed()
+    {
+    }
 }

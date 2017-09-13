2dab2e3e8370eda4156c4ea11de02db3cce7449e
LeeviPaa <leevi.paakkonen@hotmail.com>
2017-09-13 11:11:47 +0900

    added elevator and new animation assets to the game

---
+++ Assets/Scripts/Toolbox/ExampleRecieveNewPos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleRecieveNewPos : MonoBehaviour {
+
+    EventManager E;
+
+	void OnEnable()
+    {
+        E = Toolbox.RegisterComponent<EventManager>();
+        E.GiveDogDestination += RecievePos;
+    }
+    void OnDisable()
+    {
+        E.GiveDogDestination -= RecievePos;
+    }
+
+    void RecievePos(Vector3 pos)
+    {
+        Debug.LogWarning("Position recieved" + pos);
+    }
+	
+}
